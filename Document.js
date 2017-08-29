1a1ad7b7ce26e0bfe9283978c3d1324e9f92e2d9
Alex Loyko <alex.loyko96@gmail.com>
2017-08-29 18:31:58 -0400

    Added support for intersections by means of street1 and street2

--- Document.js
+++ Document.js
@@ -4,7 +4,7 @@ var validate = require('./util/valid');
 var transform = require('./util/transform');
 var _ = require('lodash');
 
-const addressFields = ['name', 'number', 'street', 'zip'];
+const addressFields = ['name', 'number', 'street', 'zip', 'street1', 'street2'];
 
 const parentFields = [
   'continent',
